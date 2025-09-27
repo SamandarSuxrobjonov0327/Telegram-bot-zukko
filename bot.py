aa007920d9a903c8b66f6a935d2f329f2a639829
samandar <149456089+SamandarSuxrobjonov0327@users.noreply.github.com>
2025-09-27 09:52:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 import asyncio
 
 API_TOKEN = "8267465259:AAHYRxwccqb-mbj5GJ6_cZqYOu3jycsVOgI"
-CHANNEL_USERNAME = "@zukko_lc"
+CHANNEL_USERNAME = "@RishLaptop"
 ADMIN_ID = 6743839154
 
 logging.basicConfig(level=logging.INFO)
